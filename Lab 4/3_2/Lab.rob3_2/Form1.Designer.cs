469e3a2836bbae33be26a401dbfa12ba7c7afdcd
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2021-12-07 01:13:27 +0300

    Add files via upload

---
+++ Lab 4/3_2/Lab.rob3_2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Lab.rob3_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Курс";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(17, 33);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(407, 331);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(445, 33);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(138, 22);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(644, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Макс знач";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(844, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Кількість категорії";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(844, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(440, 227);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 40);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Збільшити";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(644, 228);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(138, 39);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Видалити мін";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(844, 227);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(143, 39);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Перевірка";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1019, 494);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button kurs;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Lab 4/Lab3.1/Lab3.1DB/Lab3.1DB/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3._1DB
+{
+    public partial class Form1 : Form
+    {
+        List<Table> stud = new List<Table>();
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+     
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+            using (UniversityContext st = new UniversityContext())
+            {
+                List<Table> stud = st.Table.ToList<Table>();
+                dataGridView1.DataSource = stud;
+            }
+
+        }
+      
+        private async void buttonSort_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+
+            using (UniversityContext st = new UniversityContext())
+            {
+                stud = st.Table.ToList<Table>()
+                    .Where((stud) => stud.course == numCourse.Value)
+                    .ToList<Table>();  
+            }
+            dataGridView1.DataSource = stud;
+        }
+       
+        private async void Max_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+
+            using (UniversityContext st = new UniversityContext())
+            {
+                stud = st.Table.ToList<Table>()
+                    .Where((stud) => stud.name.ToLower().Contains(textFilterName.Text.ToLower()))
+                    .ToList<Table>();
+            }
+            dataGridView1.DataSource = stud;
+        }
+       
+        private async void buttonDelete_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            using (UniversityContext st = new UniversityContext())
+            {
+                stud = st.Table.ToList<Table>().OrderBy(stud => stud.rate)
+                .Skip(1)
+                .ToList<Table>();
+            }
+            dataGridView1.DataSource = stud;
+        }
+        
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+       
+        private async void MaxValueRate_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            using (UniversityContext st = new UniversityContext())
+            {
+                stud = st.Table.ToList<Table>().OrderByDescending(stud => stud.rate)
+               .Take(1)
+               .ToList<Table>();
+            }
+            dataGridView1.DataSource = stud;
+        }
+       
+        private async void count_Click(object sender, EventArgs e)
+        {
+            using (UniversityContext st = new UniversityContext())
+            {
+                MessageBox.Show($"All students: {un.Table.ToList<Table>().Count(stud => stud.course == numCourse.Value)}");
+            }
+        }
+
+        private async void buttonRate_Click(object sender, EventArgs e)
+        {
+            using (UniversityContext st = new UniversityContext())
+            {
+                MessageBox.Show($"Student were rate>60: {un.Table.ToList<Table>().Count((stud) => stud.rate>60)}");
+            }
+        }
+    }
+}
