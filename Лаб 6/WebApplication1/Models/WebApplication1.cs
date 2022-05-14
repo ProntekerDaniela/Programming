74d4fb0f0bd7a19a8e0093d0c801a0a263ecf092
ProntekerDaniela <73825709+ProntekerDaniela@users.noreply.github.com>
2022-05-14 11:55:08 +0300

    Add files via upload

---
+++ Лаб 6/WebApplication1/Models/WebApplication1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+   public class WebApplication1 : DbContext
+    {
+        public WebApplication1(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
